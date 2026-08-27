deae43ee4c50c550340fde51eb93a8905296a386
rajala avinash <abhirajala281@gmail.com>
2026-08-27 18:46:53 +0530

    Time: 8 ms (38.36%) | Memory: 20.3 MB (46.54%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        # Time: O(n)
+        # Space: O(1)
+        left, right = 0, sum(nums)
+        for index, num in enumerate(nums):
+            right -= num
+            if left == right:
+                return index
+            left += num
+        return -1
